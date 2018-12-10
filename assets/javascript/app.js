321eda111e3240323e4f511b65c81fe73cff447d
Andrew Dawson <dawso.andrew@gmail.com>
2018-12-09 22:08:50 -0500

    Adds the pause and play function to the gifs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,8 @@
+"use strict";
+
 let topics = ["Penguin", "Walrus", "Crow", "Lion", "Deer", "Bear", "Prairie Dog"];
 const apiKey = "mHPwUCR4iqfbPQYqVDWzCold4ROczEgf";
 
-function fillTopicBar() {
-  for (const topic of topics) {
-    const button = $("<button type=\"button\">");
-    button.addClass("topic");
-    button.attr("data-topic", topic);
-    button.text(topic);
-    button.on("click", () => {
-      const target = $(event.currentTarget);
-      const newTopic = target.attr("data-topic");
-      setTopic(newTopic);
-    });
-    $(".topics").append(button);
-  }
-}
 
 function buildQueryUrl(query, limit, rating) {
   let url = "https://api.giphy.com/v1/gifs/search?lang=en";
@@ -25,31 +13,65 @@ function buildQueryUrl(query, limit, rating) {
   return url;
 }
 
+function fillTopicBar() {
+  $(".topics").empty();
+
+  for (const topic of topics) {
+    const button = $("<button type=\"button\">");
+    button.addClass("topic");
+    button.attr("data-topic", topic);
+    button.text(topic);
+    button.on("click", () => {
+      const target = $(event.currentTarget);
+      const newTopic = target.attr("data-topic");
+      setTopic(newTopic);
+    });
+    
+    $(".topics").append(button);
+  }
+}
+
+function handleResponse(response) {
+  $("main").empty();
+
+  for (const result of response.data) {
+    const division = $("<div>");
+    division.addClass("gif-card");
+
+    const image = $("<img>");
+    image.attr("src", result.images.fixed_height_still.url);
+    image.attr("data-is-moving", "false");
+    image.attr("data-moving-url", result.images.fixed_height.url);
+    image.attr("data-still-url", result.images.fixed_height_still.url);
+    image.on("click", (event) => {
+      const target = $(event.currentTarget);
+      const isMoving = target.attr("data-is-moving");
+      if (isMoving === "true") {
+        target.attr("src", target.attr("data-still-url"));
+        target.attr("data-is-moving", "false");
+      } else {
+        target.attr("src", target.attr("data-moving-url"));
+        target.attr("data-is-moving", "true");
+      }
+    });
+    division.append(image);
+
+    const paragraph = $("<p>");
+    paragraph.addClass("rating");
+    paragraph.text("Rating " + result.rating.toUpperCase());
+    division.append(paragraph);
+
+    $("main").append(division);
+  }
+}
+
 function setTopic(topic) {
   const queryUrl = buildQueryUrl(topic, 10, "pg-13");
-
+  
   $.ajax({
     url: queryUrl,
     method: "GET",
-  }).then((response) => {
-    $("main").empty();
-
-    for (const result of response.data) {
-      const division = $("<div>");
-      division.addClass("gif-card");
-
-      const paragraph = $("<p>");
-      paragraph.addClass("rating");
-      paragraph.text("Rating " + result.rating.toUpperCase());
-      division.append(paragraph);
-
-      const image = $("<img>");
-      image.attr("src", result.images.fixed_height_still.url);
-      division.append(image);
-
-      $("main").append(division);
-    }
-  });
+  }).then(handleResponse);
 }
 
 
